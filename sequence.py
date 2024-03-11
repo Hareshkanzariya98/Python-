266a848fd14a446c06449735d98b08729d871009
Haresh Kanzariya <155596294+Hareshkanzariya98@users.noreply.github.com>
2024-03-11 15:20:49 +0530

    Add files via upload

---
+++ sequence.py
@@ -0,0 +1,12 @@
+#python sequnce 
+
+#String list tuple
+
+course0="PGDCA"
+print(course0[0])
+
+course0=["PGDCA","BCA","BSCIT"]
+print("List:", course0[1])
+
+course1=["PGDCA","BCA","BSCIT"]
+print("tuple:",course1[1])
